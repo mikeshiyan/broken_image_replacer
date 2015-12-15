5b326ee0f40c1f401674a754255e00da1af6e079
Mike Shiyan <dev@mike.pp.ua>
2015-12-15 08:59:25 +0200

    Replaced dummyimage service with custom CSS implementation.

--- broken_image_replacer.js
+++ broken_image_replacer.js
@@ -13,45 +13,80 @@
 
   'use strict';
 
-  if (typeof jQuery == 'undefined') {
-    // Only sites having jQuery are supported currently.
-    return;
+  function fix (image) {
+    var width  = image.width;
+    var height = image.height;
+    var text   = width + ' × ' + height;
+
+    // Calculate the font size. The final value cannot be less than 5px or more
+    // than a 3-quarters of image height.
+    var fontSize = Math.max(Math.min(width / text.length * 1.5, height * 0.75), 5);
+
+    image.alt = text;
+    // Trick the browser to show the just added 'alt' attribute.
+    image.src = null;
+    image.removeAttribute('src');
+
+    // Apply CSS.
+    image.style.backgroundImage = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAE0lEQVR42mNgAIIzZ878hxBAAAA8bwbHJ1VugQAAAABJRU5ErkJggg==")';
+    image.style.fontFamily = 'monospace';
+    image.style.fontSize = fontSize + 'px';
+    image.style.lineHeight = (height - 4) + 'px';
+    image.style.textAlign = 'center';
+
+    image.classList.add('broken-image');
   }
-
-  var $ = jQuery;
-
-  $(document).ready(function($) {
-    // Change image sources without waiting for images attempting to load.
-    if (typeof localStorage != 'undefined') {
-      var data = JSON.parse(localStorage.getItem('broken_image_replacer')) || {};
-
-      $.each(data, function (source, dimensions) {
-        $('img[src="' + source + '"]').attr('src', 'http://dummyimage.com/' + dimensions[0] + 'x' + dimensions[1] + '/cccccc/ffffff.png');
-      });
-    }
-  });
 
   var cache = {};
 
-  $('img').on('error', function () {
-    var $image = $(this);
-    var source = $image.attr('src');
+  var images = document.getElementsByTagName('img');
+  for (var i = 0, total = images.length; i < total; i++) {
+    images[i].onerror = function () {
+      // Check that image is not processed yet.
+      if (!this.classList.contains('broken-image')) {
+        cache[this.src] = 1;
+        fix(this);
+      }
 
-    if (source.indexOf('http://dummyimage.com/') == -1) {
-      var w = $image.width();
-      var h = $image.height();
+      this.onerror = null;
+    };
+  }
 
-      $image.attr('src', 'http://dummyimage.com/' + w + 'x' + h + '/cccccc/ffffff.png');
-      cache[source] = [w, h];
-    }
-  });
+  if (typeof jQuery != 'undefined') {
+    // Currently only sites having jQuery are supported for certain operations.
+    var $ = jQuery;
 
-  $(window).on('load', function() {
     if (typeof localStorage != 'undefined') {
-      var data = JSON.parse(localStorage.getItem('broken_image_replacer')) || {};
-      $.extend(data, cache);
-      localStorage.setItem('broken_image_replacer', JSON.stringify(data));
+      $(document).ready(function ($) {
+        // Change image sources without waiting for images attempting to load.
+        var data = JSON.parse(localStorage.getItem('broken_image_replacer')) || {};
+
+        $('img[src]').each(function () {
+          if (data[$(this).attr('src')]) {
+            fix(this);
+          }
+        });
+      });
+
+      var updateStorage = function () {
+        var data = JSON.parse(localStorage.getItem('broken_image_replacer')) || {};
+        $.extend(data, cache);
+        localStorage.setItem('broken_image_replacer', JSON.stringify(data));
+      };
+
+      if (typeof $(window).on == 'function') {
+        $(window).on('load', updateStorage);
+      }
+      else {
+        $(window).load(updateStorage);
+      }
     }
-  });
+
+    $(window).resize(function () {
+      $('img.broken-image').each(function () {
+        fix(this);
+      });
+    });
+  }
 
 })();
