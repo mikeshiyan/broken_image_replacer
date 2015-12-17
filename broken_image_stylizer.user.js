18dc42738126457d34aa40d669b19731a92eb2e0
Mike Shiyan <dev@mike.pp.ua>
2015-12-18 00:59:19 +0200

    Version 0.0.3

--- broken_image_stylizer.user.js
+++ broken_image_stylizer.user.js
@@ -8,7 +8,7 @@
 
 // ==UserScript==
 // @name         Broken Image Stylizer
-// @version      0.0.2
+// @version      0.0.3
 // @license      MIT
 // @description  Stylizes broken images with unobtrusive placeholder.
 // @author       Mike Shiyan
